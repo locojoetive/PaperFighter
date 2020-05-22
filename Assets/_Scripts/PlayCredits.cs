d91a5bbd6371975a0d7af2f56cfdd3748872e76d
herrnock <henok_michael@web.de>
2020-05-22 19:32:30 +0200

    NEW Controls Animation

--- Assets/_Scripts/PlayCredits.cs
+++ Assets/_Scripts/PlayCredits.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-// [ExecuteInEditMode]
+[ExecuteInEditMode]
 public class PlayCredits : MonoBehaviour
 {
     public static bool creditsHaveEnded = false;
