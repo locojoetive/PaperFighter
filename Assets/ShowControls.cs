b48feb05bbb6605790e4f1c43d47980521158a7d
locojoetive <johannes.mic@gmail.com>
2020-06-13 10:28:43 +0200

    [f-0] adds script in order to decide which controls to show

---
+++ Assets/ShowControls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class ShowControls : MonoBehaviour
+{
+
+    void Start()
+    {
+        Transform[] children = transform.GetComponentsInChildren<Transform>();
+        Transform touch = Array.Find(children, child => child.name == "Touch");
+        Transform pc = Array.Find(children, child => child.name == "PC");
+
+        touch.gameObject.SetActive(InputManager.touchActive);
+        pc.gameObject.SetActive(!InputManager.touchActive);
+    }
+}
